eeb1ee52eaa7cd5ca29924d5886adcb8178d734b
Gregory Schroeder <gregoryschroeder@Gregorys-iMac.local>
2017-10-20 17:17:10 -0400

    initial commit

---
+++ WheelPotential/Wheel/WheelVC.swift
@@ -0,0 +1,21 @@
+//
+//  WheelVC.swift
+//  WheelPotential
+//
+//  Created by Gregory Schroeder on 10/4/17.
+//  Copyright © 2017 Schroeder Studios. All rights reserved.
+//
+
+import UIKit
+
+class WheelVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        drawWheel()
+    }
+    
+    func drawWheel() {
+        
+    }
+}
