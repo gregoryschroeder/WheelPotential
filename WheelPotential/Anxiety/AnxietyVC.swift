01da56fcccbcba96c61b9fdd82d01a7615874323
gregoryschroeder <greg@schroederstudios.com>
2018-04-30 20:53:59 -0400

    UI updates. Development certificate updated

--- WheelPotential/Anxiety/AnxietyVC.swift
+++ WheelPotential/Anxiety/AnxietyVC.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2017 Schroeder Studios. All rights reserved.
 //
 
-import AVKit
 import UIKit
 
 class AnxietyVC: UIViewController {
@@ -15,27 +14,5 @@ class AnxietyVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, with: [])
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // MARK: - Navigation
-    
-    @IBAction func VideoButtonClick() {
-        guard let path = Bundle.main.path(forResource: "WheelPotential_Blu-ray_60_1080i.mpg_1", ofType:"mp4") else {
-            debugPrint("video.m4v not found")
-            return
-        }
-        let player = AVPlayer(url: URL(fileURLWithPath: path))
-        let playerViewController = AVPlayerViewController()
-        playerViewController.player = player
-        
-        self.present(playerViewController, animated: true) {
-            playerViewController.player!.play()
-        }
     }
 }
